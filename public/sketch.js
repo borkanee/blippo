d8794d6a84ee0599c71713b3ff61dc0984ad8d51
borkanee <grubesic.boris@gmail.com>
2018-04-06 17:52:38 +0200

    adds ability to generate sounds when clicking on canvas, sound is played for all users

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,6 @@
 let socket
 let cnv
+let brush
 
 // Centers the sketch canvas
 function centerCanvas() {
@@ -15,30 +16,46 @@ function windowResized() {
 
 // Setup for canvas
 function setup() {
-  cnv = createCanvas(600, 400);
+  cnv = createCanvas(700, 700)
   centerCanvas()
   background(0)
+
 
   socket = io.connect('http://localhost:3000')
   socket.on('drawing', drawNew)
 }
 
-// While draging and clicking, draw circles.
-function mouseDragged() {
+// When clicking, draw circle and play synth.
+function mousePressed() {
+  // Play only if pressed inside canvas.
+  if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
+    makeSound(mouseY)
+  }
   let drawData = {
     x: mouseX,
     y: mouseY
   }
   socket.emit('drawing', drawData)
   noStroke()
-  fill(255, 100, 100, 125)
-  ellipse(mouseX, mouseY, 20, 20)
+  fill(255, 100, 100, 100)
+  ellipse(mouseX, mouseY, 30, 30)
 }
 
 // Draw
 function drawNew(data) {
-  
+  makeSound(data.y)
   noStroke()
-  fill(255, 100, 100, 125)
-  ellipse(data.x, data.y, 20, 20)
+  fill(255, 100, 100, 100)
+  ellipse(data.x, data.y, 30, 30)
+}
+
+function makeSound(yPosition) {
+  let env = new p5.Env()
+  env.setADSR(0.1, 0.1, 0.5, 2)
+  env.setRange(0.5, 0)
+  let osc = new p5.TriOsc()
+  osc.freq(map(yPosition, 500, 0, 300, 600))
+  osc.amp(env)
+  osc.start()
+  env.play()
 }
