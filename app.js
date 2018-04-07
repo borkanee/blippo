7f1be595bbea12eb370c6ca7b283d7b486ddafbf
borkanee <grubesic.boris@gmail.com>
2018-04-07 21:07:45 +0200

    adds scales and better sound, testing saving to wav-file

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
+const fs = require("fs")
+const WavEncoder = require("wav-encoder")
 
 // Create server.
 let server = require('http').createServer(app)
@@ -18,15 +20,29 @@ let io = require('socket.io')(server)
 app.use(express.static(path.join(__dirname, 'public')))
 
 
-io.on('connection', function (socket) { 
+io.on('connection', function (socket) {
     socket.on('drawing', drawMsg)
 
     function drawMsg(data) {
         socket.broadcast.emit('drawing', data)
     }
+
+    socket.on('newSound', uploadSound)
 })
+
+function uploadSound(data) {
+    console.log(typeof data.channel1)
+    let whiteNoise1sec = {
+        sampleRate: 44100,
+        channelData: [
+            data.channel1,
+            data.channel2
+        ]
+    }
+    WavEncoder.encode(whiteNoise1sec).then(buffer => {
+        fs.writeFileSync("noise.wav", new Buffer(buffer));
+    });
+}
 
 // Listen.
 server.listen(port)
-
-
--- public/sketch.js
+++ public/sketch.js
@@ -1,6 +1,19 @@
-let socket
-let cnv
-let brush
+const pentScale = [
+  130.81, 146.83, 164.81, 196.00, 220.00,
+  261.63, 293.66, 329.63, 392.00, 440.00, 523.25
+]
+
+const majorScale = [
+  130.81, 146.83, 164.81, 174.61, 196.00, 220.00,
+  246.94, 261.63, 293.66, 329.63, 349.23, 392.00,
+  440.00, 493.88, 523.25
+]
+
+const minScale = [130.81, 146.83, 155.56, 174.61, 196.00, 207.65, 233.08, 261.63,
+  293.66, 311.13, 349.23, 392.00, 415.30, 466.16, 523.25]
+
+
+let socket, cnv, brush, noteLenght, freqInd, recorder, soundFIle, test
 
 // Centers the sketch canvas
 function centerCanvas() {
@@ -18,44 +31,73 @@ function windowResized() {
 function setup() {
   cnv = createCanvas(700, 700)
   centerCanvas()
-  background(0)
+  background("white")
+  frameRate(30)
 
-
-  socket = io()
+  socket = io.connect('http://localhost:3000/')
   socket.on('drawing', drawNew)
+
+  recorder = new p5.SoundRecorder()
+
+  // Connect the sounds from sketch to the recorder.
+  recorder.setInput()
+
+  // This sound file will be used to save the recording.
+  soundFile = new p5.SoundFile()
 }
 
 // When clicking, draw circle and play synth.
 function mousePressed() {
-  // Play only if pressed inside canvas.
-  if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
-    makeSound(mouseY)
-  }
+  recorder.record(soundFile)
   let drawData = {
     x: mouseX,
     y: mouseY
   }
+
   socket.emit('drawing', drawData)
   noStroke()
-  fill(255, 100, 100, 100)
+  fill(random(0, 256), random(0, 256), random(0, 256), 70)
   ellipse(mouseX, mouseY, 30, 30)
+
+  // Play only if pressed inside canvas.
+  if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
+    makeSound(mouseY, mouseX)
+  }
+  if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height) {
+    recorder.stop()
+    let sampleRate = soundFile.buffer.sampleRate
+    let channel1 = soundFile.buffer.getChannelData(0)
+    let channel2 = soundFile.buffer.getChannelData(1)
+    let data = { sampleRate, channel1, channel2 }
+    socket.emit('newSound', data)
+  }
 }
 
 // Draw
 function drawNew(data) {
-  makeSound(data.y)
   noStroke()
-  fill(255, 100, 100, 100)
+  fill(random(0, 256), random(0, 256), random(0, 256), 70)
   ellipse(data.x, data.y, 30, 30)
+  makeSound(data.y, data.x)
 }
 
-function makeSound(yPosition) {
+function makeSound(yPosition, xPosition) {
+  //let reverb = new p5.Reverb()
   let env = new p5.Env()
-  env.setADSR(0.1, 0.1, 0.5, 2)
+  noteLenght = floor(map(xPosition, 0, width, 1, 5))
+  env.setADSR(0.01, 1, 0.2, noteLenght)
   env.setRange(0.5, 0)
-  let osc = new p5.TriOsc()
-  osc.freq(map(yPosition, 500, 0, 300, 600))
+  let osc = new p5.SinOsc()
+  freqInd = floor(map(yPosition, height, 0, 0, minScale.length))
+  osc.freq(minScale[freqInd])
   osc.amp(env)
   osc.start()
+  //reverb.process(osc, 10, 0.15)
   env.play()
+}
+
+function blobToFile(theBlob, fileName) {
+  theBlob.lastModifiedDate = new Date();
+  theBlob.name = fileName;
+  return theBlob;
 }
